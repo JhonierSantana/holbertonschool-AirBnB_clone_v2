74cb7baca22bba80177832fbb350cf05acf24887
root <root@DESKTOP-ISQ43BP>
2023-04-05 10:18:57 -0500

    upgrade

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -8,32 +8,32 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
-    '''Prints on /'''
+    '''Second function. Prints on /hbnb'''
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def index_2():
-    '''Prints on /hbnb'''
+    '''Second function. Prints on /hbnb'''
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def index_3(text):
-    '''Prints on /c/anything'''
+    '''Third function. Prints on /c/anything'''
     return "C {}".format(text).replace('_', ' ')
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def index_4(text='is cool'):
-    '''Prints on /python/anything'''
+     '''Fourth function. Prints on /python/anything'''
     return "Python {}".format(text).replace('_', ' ')
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def index_4(n):
-    ''' Prints on number if n is int '''
+    '''Fifth function. Prints on number if n is int'''
     return "{} is number".format(n)
 
 
