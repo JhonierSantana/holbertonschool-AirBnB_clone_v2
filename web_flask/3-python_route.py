11c93dbffc0b93c3abb456ebf25116011146725c
root <root@DESKTOP-ISQ43BP>
2023-04-05 09:37:40 -0500

    upgrade

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+''' Star flask'''
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def index_2():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def index_3(text):
+    return "C {}".format(text).replace('_', ' ')
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def index_4(text):
+    text = "is cool"
+    return "Python {}".format(text).replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
