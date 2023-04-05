f0433c396985152fe20987c5c01927cc90b86f6f
root <root@DESKTOP-ISQ43BP>
2023-04-05 10:16:16 -0500

    upgrade

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -8,22 +8,26 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
+    '''Prints on /'''
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def index_2():
+    '''Prints on /hbnb'''
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def index_3(text):
+    '''Prints on /c/anything'''
     return "C {}".format(text).replace('_', ' ')
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def index_4(text='is cool'):
+    '''Prints on /python/anything'''
     return "Python {}".format(text).replace('_', ' ')
 
 
