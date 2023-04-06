6696d4d6b191de52df8a885219d8f2d82a03efdf
root <root@DESKTOP-ISQ43BP>
2023-04-06 09:39:15 -0500

    upgrade

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+''' Star flask'''
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_close(self):
+    '''Closses sqlalchemy session'''
+    storage.close
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Returns a HTML with states list"""
+    state_dict = storage.all(State)
+    ret_list = []
+    for state in state_dict.values():
+        ret_list.append(state)
+    return render_template('7-states_list.html', st_list=ret_list)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
