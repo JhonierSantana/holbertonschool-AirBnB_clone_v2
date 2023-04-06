5fd0cfa72521407d5b9632fe5239b3dbdbec1987
root <root@DESKTOP-ISQ43BP>
2023-04-06 10:02:40 -0500

    upgrade

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""First flask module for HBNB"""
+
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from os import getenv
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_close(self):
+    "Closses sqlalchemy session"
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_state_list():
+    """Returns a HTML with states list"""
+    state_list = storage.all(State).values()
+    return render_template('8-cities_by_states.html', st_list=state_list)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
