64a5db40223c2f9da4a2a5975c13211e51fbc7be
root <root@DESKTOP-ISQ43BP>
2023-04-07 15:40:53 -0500

    upgrade

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,28 +1,24 @@
 #!/usr/bin/python3
-"""First flask module for HBNB"""
-
-
+''' starts a Flask web application'''
 from flask import Flask, render_template
-from models import storage
 from models.state import State
+from models import storage
+
+
 app = Flask(__name__)
 
 
+@app.route('/states_list', strict_slashes=False)
+def st_list():
+    st = storage.all(State).values()
+    return render_template('7-states_list.html', st=st)
+
+
 @app.teardown_appcontext
-def teardown_close(self):
-    "Closses sqlalchemy session"
+def teardown_db(exception):
+    '''remove the current SQLAlchemy Session'''
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """Returns a HTML with states list"""
-    state_dict = storage.all(State)
-    ret_list = []
-    for state in state_dict.values():
-        ret_list.append(state)
-    return render_template('7-states_list.html', st_list=ret_list)
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
